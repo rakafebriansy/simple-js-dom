1f780659d3e150bb23267cfd6c99f771f1f7964a
rakafebriansy <kerenr445@gmail.com>
2024-03-23 23:57:05 +0700

    revisi 1

--- src/script.js
+++ src/script.js
@@ -6,7 +6,7 @@ IYA.addEventListener('click', () => {
     });
     document.querySelector('.wrapper > div').style.backgroundColor = '#6FD240';
     document.querySelector('.wrapper h1').innerText = 'Anda Benar!';
-    document.querySelector('.wrapper > div > div > div:first-child').innerText = 'Anda adalah seorang Mahasiswa';
+    document.querySelector('.wrapper > div > div > div:first-child > p').innerText = 'Anda adalah seorang Mahasiswa';
     document.querySelector('.wrapper > div > img').setAttribute('src','assets/22b0da421086d344420bcc7fb45598b1.png')
 });
 TIDAK.addEventListener('click', () => {
@@ -15,6 +15,6 @@ TIDAK.addEventListener('click', () => {
     });
     document.querySelector('.wrapper > div').style.backgroundColor = '#E12E2E';
     document.querySelector('.wrapper h1').innerText = 'Anda Berbohong!';
-    document.querySelector('.wrapper > div > div > div:first-child').innerText = 'Anda adalah seorang Teknisi';
+    document.querySelector('.wrapper > div > div > div:first-child > p').innerText = 'Anda adalah seorang Teknisi';
     document.querySelector('.wrapper > div > img').setAttribute('src','assets/bcb2a8115cc9610a0c1a9d78ccb9a051.png')
 });
